dc5969ac13b5ddc19d083ccb8647a872e167e055
TomaMajic <toma.majic@fer.hr>
2018-01-13 12:00:12 +0100

    Add web3 injection and minor ui changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(() => {
+	if(typeof web3 !== undefined) {
+		window.web3 = new Web3(web3.currentProvider);
+	} else {
+		console.log('No injected web3 found');
+
+		var provider = 'http://localhost:8545';
+		window.web3 = new Web3(new Web3.providers.HttpProvider(provider));
+	}
+
+	console.log('Web3 provider: ', window.web3.currentProvider);
+});
