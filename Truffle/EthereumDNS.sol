cb312964520939fbb49bc0d83d2099868c784067
jPaladin <josip.paladin@fer.hr>
2018-01-17 00:28:40 +0100

    init struct

--- Truffle/EthereumDNS.sol
+++ Truffle/EthereumDNS.sol
@@ -1,4 +1,10 @@
 pragma solidity ^0.4.0;
 contract EthereumDNS{
+    // Define struct for reserving domain
+    struct DNSRecord {
+        address owner_addr;
+        string ip_addr;
+        uint end_timestamp;
+    }
    
 }
