7ceb14b53531474597dbb1b08b656a596c3ee020
jPaladin <josip.paladin@fer.hr>
2018-01-17 00:54:58 +0100

    added mapping

--- Truffle/EthereumDNS.sol
+++ Truffle/EthereumDNS.sol
@@ -2,9 +2,16 @@ pragma solidity ^0.4.0;
 contract EthereumDNS{
     // Define struct for reserving domain
     struct DNSRecord {
+        // Ethereum address for owner of the domain
         address owner_addr;
+        // IP address attached to domain
         string ip_addr;
+        // Time remaining before record is not valid
         uint end_timestamp;
     }
+    
+    // List containing domains (string) and their owners
+    mapping (string => DNSRecord) dns_records;
+
    
 }
