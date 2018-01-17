f243b351f78870776883547dbb6f5908722c88f0
jPaladin <josip.paladin@fer.hr>
2018-01-17 01:42:17 +0100

    private calculate time for amount

--- Truffle/EthereumDNS.sol
+++ Truffle/EthereumDNS.sol
@@ -7,14 +7,20 @@ contract EthereumDNS{
         // IP address attached to domain
         string ip_addr;
         // Time remaining before record is not valid
-        uint end_timestamp;
+        uint256 end_timestamp;
     }
     
-    // Define current price for a year in wei
+    // Define current price for a day in wei
     uint private CurrentPrice = 1;
     
     // List containing domains (string) and their owners
     mapping (string => DNSRecord) dns_records;
-
-   
+    
+    // Fail safe for hackers - check amount against our current price
+    function calculateTimeForAmount(uint amount) private returns (uint256){
+        // Calculate number of days (no decimals)
+        var numberOfDays = amount / CurrentPrice;
+        // Return number of seconds 
+        return numberOfDays * 86400;
+  }
 }
